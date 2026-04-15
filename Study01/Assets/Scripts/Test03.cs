80d7fd96c9ff00b2ce8ac5cedd4f63dcf1a004b0
soyoonL <diamomo777@naver.com>
2026-04-15 15:06:26 +0900

    [feat] while 반복문 연습

--- Study01/Assets/Scripts/Test03.cs
+++ Study01/Assets/Scripts/Test03.cs
@@ -5,37 +5,21 @@ using UnityEngine;
 
 public class Test03 : MonoBehaviour
 {
-    // 제어문 : 상황에 따라 명령어의 실행을 선택하고 순서를 바꾸거나 반복시키는 명령
-    // case는 연달아 사용 가능, 즉 첫번째 케이스나 두 번쩨 케이스가 해당할 경우 아래로 넘어가게 하는 방향으로 작성 가능
+    // 반복문 :  특정 조건을 만족할 경우 원하는 명령을 계속해서 반복하도록 하는 명령
+    // while : 특정 조건을 만족하는 동안 반복
+    // do while : 조건에 맞든 안 맞든 일단 한번은 실행하고 조건 비교
 
-    //중첩 if문, 안으로 파고들며 검사하는 방식 
-    // ex) 1900 > 4로 나누어떨어짐? ㅇㅇ > 100으로? ㅇㅇ > 400으로? ㄴㄴ > 평년
-    public int year;
-   
+    float kg = 48.5f;
+    int count = 0;
     void Start()
     {
-        if (year % 4 == 0)
+       while(kg <= 90)
         {
-            if(year%100 == 0)
-            {
-                if(year%400==0)
-                {
-                    Debug.Log("윤년입니다");
-                }
-                else
-                {
-                    Debug.Log("평년입니다");
-                }
-            }
-            else
-            {
-                Debug.Log("윤년입니다");
-            }
+            kg += 0.5f;
+            count++;
         }
-        else
-        {
-            Debug.Log("평년입니다");
-        }
+
+       Debug.Log ("햄버거를 " + count + "번 먹었더니 살이 쪘어 ㅠ");
     }
 
  
