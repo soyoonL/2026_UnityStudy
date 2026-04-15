956d3adf38a809145d8a952878ace8862023b673
soyoonL <diamomo777@naver.com>
2026-04-15 13:17:00 +0900

    [feat] 숫자가 양수인지, 0인지, 음수인지 출력하는 프로그램

--- Study01/Assets/Scripts/Test03.cs
+++ Study01/Assets/Scripts/Test03.cs
@@ -6,33 +6,24 @@ using UnityEngine;
 public class Test03 : MonoBehaviour
 {
     // 제어문 : 상황에 따라 명령어의 실행을 선택하고 순서를 바꾸거나 반복시키는 명령
-
-    int age = 28;
-    string Movie = "LastHoliday";
+    // case는 연달아 사용 가능, 즉 첫번째 케이스나 두 번쩨 케이스가 해당할 경우 아래로 넘어가게 하는 방향으로 작성 가능
+    public int number;
+   
     void Start()
     {
-        if (age < 20) // 조건식이 true면 실행
+        if (number > 0)
         {
-            Debug.Log("애기");
+            Debug.Log("이 수는 양수입니다");
         }
-        else if (age < 30) 
+        else if(number < 0)
         {
-            Debug.Log("애기겠냐");
+            Debug.Log("이 수는 음수입니다");
         }
-
-        switch (Movie)
+        else
         {
-            case "HarryPorter":
-               
-            case "KpopDemonHunters":
-                Debug.Log("위 고잉 업업");
-                break;
-            default:
-                Debug.Log("누구쎄용");
-                break;
+            Debug.Log("이 수는 0입니다");
         }
-
-        // case는 연달아 사용 가능, 즉 첫번째 케이스나 두 번쩨 케이스가 해당할 경우 아래로 넘어가게 하는 방향으로 작성 가능
+        
     }
 
  
