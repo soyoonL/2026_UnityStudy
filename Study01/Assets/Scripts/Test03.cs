ee89b020778ede2a670a0101995376a505fc1d35
soyoonL <diamomo777@naver.com>
2026-04-15 14:00:19 +0900

    [Feat] 연산기호와 두 개의 숫자를 넣고 연산 기호에 따라 두 수를 계산해서 결과를 출력하는 프로그램

--- Study01/Assets/Scripts/Test03.cs
+++ Study01/Assets/Scripts/Test03.cs
@@ -9,25 +9,29 @@ public class Test03 : MonoBehaviour
     // case는 연달아 사용 가능, 즉 첫번째 케이스나 두 번쩨 케이스가 해당할 경우 아래로 넘어가게 하는 방향으로 작성 가능
 
     // 핵심은 좁은 조건(400)부터 먼저 검사하고, 넓은 조건(4)을 나중에 검사하는 것!
-    public int year;
+    public int a;
+    public int b;
+    public string cal;
    
     void Start()
     {
-        if (year % 400 ==0)
+        switch (cal)
         {
-            Debug.Log("윤년입니다");
-        }
-        else if (year % 100 == 0)
-        {
-            Debug.Log("평년입니다");
-        }
-        else if (year % 4 == 0)
-        {
-            Debug.Log("윤년입니다");
-        }
-        else
-        {
-            Debug.Log("평년입니다");
+            case "+":
+               Debug.Log(a+b);
+               break;
+            case "-":
+                Debug.Log(a - b);
+                break;
+            case "*":
+                Debug.Log(a * b);
+                break;
+            case "/":
+                Debug.Log(a / b);
+                break;
+            default:
+                Debug.Log("연산기호를 넣어주세요");
+                break;
         }
     }
 
