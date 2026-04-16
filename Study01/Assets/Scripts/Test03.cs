3526cbb9cf2c2fde45590f768e755a8d76421150
soyoonL <diamomo777@naver.com>
2026-04-16 14:36:09 +0900

    [feat] 1보다 큰 수를 변수 a,b에 각각 넣고 a에 b를 몇번 곱하면 1000을 넘는지 알아내는 프로그램

--- Study01/Assets/Scripts/Test03.cs
+++ Study01/Assets/Scripts/Test03.cs
@@ -13,24 +13,32 @@ public class Test03 : MonoBehaviour
     // while(true) : 무한반복, 추천하지 않음
     // continue : continue를 만나는 순간 반복문 안에 있는 다음 문장들은 무시하고 다시 올라감
     // for(초기화 ;반복 조건; 반복명령어
-    
 
+    public int a;
+    public int b;
+    private int count = 0;
     private void Start()
     {
-        for(int i = 0; i < 10; i++)
+        if(a>1 && b > 1)
         {
-            string stars = "";
-
-            for(int j = 0; j< i; j++)
+            int result = a;
+            while (result <= 1000)
             {
-                stars += "*";
+               result = result * b;
+               count++;
+                
             }
 
-            Debug.Log(stars);
+            Debug.Log(a + "에" + b + "를" + count + " 번 곱하면 1000을 넘습니다.");
+        }
+        else
+        {
+            Debug.Log("1보다 큰 수를 입력해주세요.");
         }
 
-        // i=0이면 j<i가 성립이 안됨(0<0). 그러므로 처음에는 빈 칸으로 나옴
-        // i=1이면 0<1로 성립이 되므로 * 출력, 1<1은 성립이 안되므로 출력 더 이상 안됨
-        // string stars = "" 변수를 만드는 이유는 별을 중첩시키지 않기 위해서이다. 매 줄마다 빈 상자로 초기화시켜 차례대로 피라미드가 나오게 한다
     }
+
+    // int result = b : result 변수에 b의 값을 넣음.
+    // result = result * b : 예를 들어 a가 2, b가 3이라고 할 때, 2*3=6,6*3(2*3*3) =18 이런 식으로 
+    // result <= 1000 : 결과가 1000보다 작거나 같을 때(a*b를 변수로 간단하게 표현하는 방식이 좋은 거 같음)
 }
