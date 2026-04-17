9159bd985c52fe8a8db5dd9b115e7852425309fc
soyoonL <diamomo777@naver.com>
2026-04-17 17:09:54 +0900

    [feat] 2차원 배열을 이용한 프로그램

--- Study01/Assets/Scripts/Test03.cs
+++ Study01/Assets/Scripts/Test03.cs
@@ -11,22 +11,23 @@ public class Test03 : MonoBehaviour
 
     private void Start()
     {
-        // 크기가 5인 int 배열을 만들고, for문을 이용해 각 칸에 10, 20, 30, 40, 50을 넣은 뒤, 배열의 모든 값을 더한 합계를 Debug.Log로 출력하는 프로그램을 작성하세요.
+        // 3줄 × 3칸짜리 2차원 배열을 만들어서 구구단 7단의 7×1부터 7×9까지의 결과값을 순서대로 저장하고, 이중 for문으로 모든 값을 Debug.Log로 출력하는 프로그램을 작성하세요.
 
-        int[] a = new int[5] ;
+        int[,] a  = new int[3, 3];
+        int count = 1;
 
-        for(int i  = 0; i <5 ; i++)
+        for (int i = 0;  i < 3; i++)
         {
 
-            a[i] = 2 * 5 * i + 10;
-
+            for(int j = 0; j < 3; j++)
+            {
+                a[i, j] = 7 *count;
+                count++;
+                Debug.Log(a[i, j]);
+            }
+           
         }
 
-        Debug.Log(a[0] + a[1] + a[2] + a[3] + a[4]);
-        Debug.Log(a[0]);
-        Debug.Log(a[1]);
-        Debug.Log(a[2]);
-        Debug.Log(a[3]);
-        Debug.Log(a[4]);
+
     }
 }
