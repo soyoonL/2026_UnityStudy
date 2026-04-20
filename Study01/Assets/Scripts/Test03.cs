43554d6b4d79e3eb940d689b2b1d1f2514e86d03
soyoonL <diamomo777@naver.com>
2026-04-20 21:14:45 +0900

    [feat] foreach_배열로 연습

--- Study01/Assets/Scripts/Test03.cs
+++ Study01/Assets/Scripts/Test03.cs
@@ -8,45 +8,29 @@ using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCou
 
 public class Test03 : MonoBehaviour
 {
-    // 컬렉션 : 하나의 이름 아래 여러 개의 데이터를 묶어서 관리하는 것
-    // 배열과 비슷하지만 다름
-    // 컬렉션은 배열과 달리 크기가 정해져있지 않음
-    // 배열은 처음에 방을 만들 때 방의 개수를 정해놓고 얘기했지만, 컬렉션은 실시간으로 계속해서 방의 개수를 변화시킬 수 있음.
-    // Dictionary, List
-
+    // foreach : 배열이나 컬렉션 안의 모든 원소를 한 번씩 꺼내어 쭉 써보겠다는 의미
+    // foreach( 데이터타입 변수명 in 배열명 ){}
+    // foreach( 데이터타입 변수명 in 컬렉션명 ){}
     private void Start()
     {
-        // Dictionary : 사전
-        // 우리가 사전에서 단어를 찾으면 단어와 단어에 관한 내용이 서술되어 있다. 여기서 단어는 Key, 내용은 Value 라고 볼 수 있다.
-        // Key값으로 보통 문자열을 많이 사용하지만 숫자가 들어가도 괜춘
+        // 가변배열
+        int[][] a = new int[5][];
+        a[0] = new int[5] { 1, 2, 3, 4, 5 };
+        a[1] = new int[5] { 6, 2, 3, 4, 5 };
+        a[2] = new int[5] { 7, 2, 3, 4, 5 };
+        a[3] = new int[5] { 8, 2, 3, 4, 5 };
+        a[4] = new int[5] { 9, 2, 3, 4, 5 };
 
-        // Key값으로 string, value 값으로 string을 가지는 Dictionary
-        // Dictionary는 Key 값으로 값을 가져오기 때문에 순서를 알 수 없으므로 foods[0] 이런 식으로 값을 가져오는 것이 안되며 foods.Insert도 사용이 안된다.
-        Dictionary<string,string> foods = new Dictionary<string,string>();
-
-        foods.Add("한국", "약과");
-        foods.Add("일본", "당고");
-        foods.Add("중국", "탕후루");
-        foods.Add("미국", "케이크쉐이크");
-
-        // 중간에 값을 교체하는 것도 가능
-        // Key 값이 같은 두 개의 value는 존재할 수 없다
-        foods["한국"] = "개성주악";
-        Debug.Log(foods["한국"]);
-
-        // ContainsKey : 이 Key를 가지고 있는 원소가 있는지
-        Debug.Log(foods.ContainsKey("미국"));
-
-        // ContainsValue : Value 중에 이러한 Value가 존재하는지
-        Debug.Log(foods.ContainsValue("당고"));
-
-        // Remove : Key 값을 통해서 삭제를 함
-        // Clear도 가능
-        foods.Remove("일본");
-        foods.Clear();
-
-
+        foreach (int[] numbers in a)
+        {
+            foreach (int i in numbers)
+            {
+                Debug.Log(i);
+            }
+           
+        }
     }
+
 
 
 }
